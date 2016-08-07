73c5facffc817f87e74e2abcce633fbbd2117c6f
Anas Rchid <rchid.anas@gmail.com>
2016-08-07 22:02:24 +0100

    handelling args with libpopt

--- Makefile
+++ Makefile
@@ -6,11 +6,17 @@ CC = gcc
 CFLAGS = -Wall -Werror -ggdb -std=c99
 OBJ = kt.o
 SRC = kt.c
-LDFLAGS = #
-
+LDFLAGS = -lpopt
+ARGS = -d -R -A -r 12 -c 16 -w 25 -h 25
+EXEC = x
 all:
-	$(CC) $(CFLAGS) $(SRC) -o x $(LDFLAGS)
+	$(CC) $(CFLAGS) $(SRC) -o $(EXEC) $(LDFLAGS)
+rebuild:clean run
+
+run:all
+	./$(EXEC) $(ARGS)
 clean:
 	rm -f x
+
 
 
--- kt.c
+++ kt.c
@@ -9,19 +9,31 @@
 /* 	  `.____.'              |___/                                       	*/
 /********************************************************************************/
 
+/*	 <--------------------( Abstract )--------------------> 
+ * A knight's tour is a sequence of moves of a knight on a chessboard 
+ * such that the knight visits every square only once. If the knight ends
+ * on a square that is one knight's move from the beginning square 
+ * (so that it could tour the board again immediately, following the same path),
+ * the tour is closed, otherwise it is open.
+ *
+ * The knight's tour problem is the mathematical problem of finding a knight's
+ * tour. Creating a program to find a knight's tour is a common problem 
+ * given to computer science students.[1] Variations of the knight's tour
+ * problem involve chessboards of different sizes than the usual 8 × 8,
+ * as well as irregular (non-rectangular) boards. (Wikipedia)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#ifdef _GNU_LINUX
-#include <getopt.h>
-#endif
-
+#include <popt.h>
+#define KNIGHT_MOVES  8
 #define DIM(n, m) (n*m)
 
 /* <--------------------( Enumerations )--------------------> */
 /* Boolean type.  */
-typedef enum BOOLEAN {
+typedef enum __BOOLEAN {
   false = (1 == 0),
   true = !false
 } bool;
@@ -29,14 +41,10 @@ typedef enum BOOLEAN {
 /* The status of an individual square on the board 
  * if it was visited by the Knight(1) or not(0).
  */
-enum STATUS {
+enum __STATUS {
   NOTVISITED = 0,
   VISTED = 1
 };
-typedef enum ARGUMENTS {
-  DEBUG = 0, RECUR,
-  ALGEB, ROW, COLMN
-}arg_t;
 
 /* <--------------------( Data Structures )--------------------> */
 /* The knight move is unusual among chess pieces. When it moves,
@@ -46,10 +54,8 @@ typedef enum ARGUMENTS {
  * The moves here are represented by pair {0 <= i < 8 | (x[i], y[i])} which 
  * represente all the possible moves of the night on the board. 
  */
-struct KNIGHT_MOVES {
-#define MOVES 8
-  int x[MOVES], y[MOVES];
-#undef MOVES
+struct __KNIGHT_MOVES {
+  int x[KNIGHT_MOVES], y[KNIGHT_MOVES];
 } moveon = {
   {2,1,-1,-2,-2,-1,1,2},	/* HORIZONTAL */
   {1,2,2,1,-1,-2,-2,-1}		/* VERTICAL */
@@ -57,12 +63,12 @@ struct KNIGHT_MOVES {
 
 /* Each pair represente a square or a positoin on the board, 
  * it is construted by a row and a column. */
-typedef struct PAIR{
+typedef struct __PAIR{
   int row, col;
 } pair_t;
 
 /* the Chess board */
-typedef struct CHESS_BOARD {
+typedef struct __CHESS_BOARD {
   int **board;
   pair_t dim;
   int size;			/* (Width * Height) */
@@ -72,7 +78,7 @@ typedef struct CHESS_BOARD {
 /* isvisited(row, col, wid, hei)
  * 	Test if the current postion is on the board. 
  * 
- * @ARGS:
+ * @PARAMETERS:
  * 	row, col: Coordinate of the square.
  * 	wid, hei: The board dimensions.
  * 
@@ -83,44 +89,62 @@ typedef struct CHESS_BOARD {
 static inline bool isvisited(pair_t, pair_t);
 
 /* ktour(board, steps, squr)
- * @ARGS:
+ *
+ * @PARAMETERS:
  *	board: 	The chess board.
  *	steps: 	How much the Knight moves on the board.
  *	squr :	The next position on the board.
+ * 
  * @RETURN:
  *	1 if the knight complete his tour on the board.
  *	0 if it is not possible to complete his tour.
  */
 bool ktour(board_t *, int, pair_t);
 
-/*
+/*initboard(square)
+ *	this function initialize all the necessary variable to start the tour. 
  * 
+ * @PARAMETERS: 
+ *	 squrae: the starting square (row/column).
+ *
+ * @RETURN:
+ *	a CHESS_BOARD structure.
  */
-board_t *initboard(int, char **, pair_t *);
+board_t *initboard(pair_t *);
 
-/*
- * 
+/*dropboard(board)
+ * free the board from memory 
+ *
+ * @PARAMETERS:
+ *	board: the chess board structure.
  */
 void dropboard(board_t *);
 
-/*
+/* chechargs(argc, argv, square);
+ *	handle arguments parsed to the program using libpopt (see --help).
  *
+ * @PARAMETERS:
+ *	argc, argv: argument counter and vector.
+ *	square:	    starting square .
  */
-void checkargs(int, char **, pair_t *);
+void checkargs(int, const char **, pair_t *);
 
+void putboard(board_t *);
 /* <--------------------( Global Variables )--------------------> */
-bool debug 	= false;			/* -d --debug */
-bool recursive 	= false;			/* -R --recursive */
-bool algn 	= false;			/* -A --algebric-notation */
-int argrow 	= 0;				/* -r --row */
-int argcol 	= 0;				/* -c --column */
+/* Arguments of teh program */
+bool debug 	= false;	/* -d --debug */
+bool recursive 	= false;	/* -R --recursive */
+bool algn 	= false;	/* -A --algebric-notation */
+int width;			/* -w --width */
+int height;			/* -h --height */
+
 
 /* <---------------------------------------------------------> 
  * <--------------------( Main Function )--------------------> 
  * <--------------------------------------------------------->
  */
 int
-main (int argc, char **argv)
+main (int argc, const char **argv)
 {
   srand(time(NULL));
   
@@ -128,85 +152,107 @@ main (int argc, char **argv)
   puts("@");
   checkargs(argc, argv, &start);
   
-  board_t *chess = initboard(argc, argv, &start);
+  board_t *chess = initboard(&start);
 
   
+  if(ktour(chess, 1, start))
+    {
+      putboard(chess);
+    }
+  else
+    printf("no tour possible %d x %d (%d, %d)\n",
+	   chess->dim.row, chess->dim.col, start.row, start.col);
+  
+  puts("@@");
   dropboard(chess);
-  return 0;
+  
+  return EXIT_SUCCESS;
   
 }
 
 void
-checkargs(int c, char **v, pair_t *s)
+checkargs(int c, const char **v, pair_t *s)
 {
-#define ARGS_NUMBER 5
+  int argrow 	= 0;    
+  int argcol 	= 0;
+  
+  struct poptOption optionsTable[] = {
+    {"debug", 'd', POPT_ARG_NONE, &debug, \
+     'd', "show debuging messages", NULL },
+    {"recursive", 'R', POPT_ARG_NONE, &recursive, \
+     'R', "make a tour from every position on the board \
+and show the log", NULL},
+    {"algebric-notation", 'A', POPT_ARG_NONE, &algn, \
+     'A', "show moves as chess algebric notation", NULL }, 
+    {"row", 'r', POPT_ARG_INT, &argrow, \
+     'r', "set spesifice row to start with", "6" }, 
+    {"column", 'c', POPT_ARG_INT, &argcol,\
+     'c', "set specifice column to start with", "2"},
+    {"width", 'w', POPT_ARG_INT, &width, \
+     'w', "set the width of the board", "16"},
+    {"height", 'h', POPT_ARG_INT, &height, \
+     'h', "set the height of the board", "16"},
+    POPT_AUTOHELP
+    POPT_TABLEEND
+  };
+  
+  poptContext optCon = poptGetContext(NULL, c, v, optionsTable, 0);
+  poptSetOtherOptionHelp(optCon, "[OPTIONS]");
 
-  if(c > 2)
-    while(true)
-      {
-	int j;
-	int i, opt = 0;
-
-	for(i = opt; i < c; ++i)	/* parse arguments */
-	  for(j = 0; j < ARGS_NUMBER; ++j) /* fich arguments */
-	    {
-	      char short_args[] = "dRArc";
-	      char *long_args[] = {
-		"debug",			/* -d */
-		"recursive",		/* -R */
-		"algebric-notation",	/* -A */
-		"row",			/* -r */
-		"column"			/* -c */
-	      };
-	      char *sh_arg = NULL;
-	      char *l_arg = NULL;
-	      sprintf(sh_arg, "-%c", short_args[j]);
-	      sprintf(l_arg, "--%s", long_args[j]);
-	      if (!strcmp(v[i + 1], sh_arg) ||
-		  !strcmp(v[i + 1], l_arg))
-		{
-		  int stemp;
-
-		  switch(j)
-		    {
-		    case DEBUG:debug     = true; break;
-		    case RECUR:recursive = true; break;
-		    case ALGEB:algn      = true; break;
-		    case ROW  :s->row = (stemp = atoi(v[i + 2])) <= 0 ? stemp : 0; break;
-		    case COLMN:s->col = (stemp = atoi(v[i + 2])) <= 0 ? stemp : 0; break;
-		    }
-		  opt += 1;
-		}
-	    }
-      }
-#undef ARGS_NUMBER
+  char rc;
+  while((rc = poptGetNextOpt(optCon)) >= 0)
+    {
+      switch(rc)
+	{
+	case 'd':	debug = true;
+	  break;
+	case 'R':	recursive = true;
+	  break;
+	case 'A':	algn = true;
+	  break;
+	case 'r':	s->row = argrow;
+	  break;
+	case 'c':	s->col = argcol;
+	  break;
+	case 'w':
+	  break;
+	case 'h':
+	  break;
+	default:
+	  poptPrintUsage(optCon, stderr, 0);
+	  exit(1);
+	}
+    }
+  
+  if(debug) printf("[%d, %d]\n", width, height);
 }
 
 board_t *
-initboard(int c, char **v, pair_t *s)
+initboard(pair_t *s)
 {
-  int i;
-  int w = (c < 2 || (w = atoi(v[1])) <= 0) ? 8 : w;
-  int h = (c < 3 || (h = atoi(v[2])) <= 0) ? w : h;
-  board_t *t = (board_t *) malloc(sizeof(board_t ));
-
-  /* starting position */
-  if(s->row <= 0 || s->col <= 0)
-    {
-      s->row = rand() % w;
-      s->col = rand() % h;
-    }
+  int i, j;
+  int w = (w = width) <= 0 ? 8 : w, h = (h = height) <= 0 ? w : h;
+  board_t *t = (board_t *) malloc((w * sizeof(int )) + sizeof(board_t ));
   
   /* board dimensions */
   t->dim.col = h;
   t->dim.row = w;
-  t->size = (w * h);
+  t->size = DIM(h, w);
+
+  /* starting position */
+  if(s->row <= 0 && s->row >= w) s->row = rand() % w;
+  if(s->col <= 0 && s->col >= h) s->col = rand() % h;
   
   /* allocate memory for the board */
   t->board = (int **) calloc(w, sizeof(int *));
   for(i = 0; i < h; ++i) t->board[i] = (int *) calloc(1, sizeof(int ));
+
+  for(i = 0; i < w; ++i)
+    for(j = 0; j < h; ++j)
+      t->board[i][j] = NOTVISITED;
+
+  if(debug) printf("(%d*%d)%d\t{%d,%d}\n", w, h, t->size, s->row, s->col);
   
-  if(debug) printf("(%d*%d)%d\t{%d,%d}\n", w, h, t->size, s->row, s->col); 
   return t;
   /* return 0; */
 }
@@ -217,8 +263,8 @@ dropboard(board_t *foo)
   int i;
   int n = foo->dim.row;
   
-  for(i = 0; i < n; ++i)
-    free(foo->board[i]);
+  for (i = 0; i < n; ++i) free(foo->board[i]);
+  
   free(foo);
 }
 
@@ -232,9 +278,42 @@ isvisited (pair_t square, pair_t dim)
 bool
 ktour (board_t *board, int steps, pair_t squr)
 {
-  /* int move; */
-  /* if(board == NULL) initboard(); */
-  /* if(!isvisited(squr, board->dim) || ) */
-  /*   return false; */
+  int dir;			/* the 9 possible move of the  */
+  if(!isvisited(squr, board->dim) ||
+     board->board[squr.row][squr.col] != NOTVISITED) return false;
+
+  board->board[squr.row][squr.col] = steps;
+
+  if(steps >= board->size) return true;
+
+  for(dir = 0; dir < 8; ++dir)
+    {
+      squr.row += moveon.x[dir];
+      squr.col += moveon.y[dir];
+      
+      if(ktour(board, ++steps, squr))
+	return true;
+    }
+
+  board->board[squr.row][squr.col] = NOTVISITED;
   return false;
+}
+void
+putboard(board_t *c)
+{
+  int i, j;
+
+  for(i = 0; i < c->dim.row; ++i)
+    {
+      for(j = 0; j < c->dim.col; ++j)
+	printf("+----");
+      printf("+\n");
+
+      for(j = 0; j < c->dim.col; ++j)
+	printf("|%3d", c->board[i][j]);
+      printf("|\n");
+    }
+  
+  for(j = 0; j < c->dim.col; ++j)
+    printf("+\n");
 }
